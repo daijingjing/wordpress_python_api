6b19a3cac5fc040ea52eb4a5a56e8e72b817bf88
daijingjing <jjyyis@qq.com>
2016-09-18 19:43:55 +0800

    hehe

--- Server.py
+++ Server.py
@@ -4,6 +4,11 @@
 import tornado.ioloop
 import tornado.web
 
+
+def get_category():
+    sql = "SELECT `wp_term_taxonomy`.`taxonomy`,`wp_term_taxonomy`.`parent`,`wp_terms`.* FROM `wp_term_taxonomy`,`wp_terms` WHERE `wp_term_taxonomy`.`term_id`=`wp_terms`.`term_id` AND `wp_term_taxonomy`.`parent`=%s ORDER BY `wp_terms`.`term_order`"
+
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
