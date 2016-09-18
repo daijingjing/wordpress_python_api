d61ab06ba357b84d766050a1c0ae43014b5268cd
root <root@myserver.(none)>
2016-09-18 19:26:27 +0800

    创建服务主程序

---
+++ Server.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+if __name__ == "__main__":
+    application = tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+    application.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
+
